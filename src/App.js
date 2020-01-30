9f8e26b49efd308f28708b2a957fc05c832f76c0
April Ma <54913304+april5622@users.noreply.github.com>
2020-01-29 18:04:01 -0800

    login setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import './App.css';
 import Home from './Components/home';
+import login from './Components/login'
 
 function App() {
-  return (
+  return ( <>
     <Route exact path ="/" component={Home} />
+    <Route path="/login" component={login} />
+    </>
   );
 }
 
