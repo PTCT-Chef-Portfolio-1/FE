0dc66b99489fde1f74f16d2069218370053d2f76
April Ma <54913304+april5622@users.noreply.github.com>
2020-02-03 18:26:29 -0800

    saved

--- src/Components/recipeCard.js
+++ src/Components/recipeCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
-// import Card from '@material-ui/core/Card';
+import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
@@ -38,10 +38,9 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function recipeCard() {
+export default function recipeCard({recipe_name, recipe_photo, ingredients, cook_time, prep_time, instructions, servings}) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
-  const {recipe_name, recipe_photo, ingredients, cook_time, prep_time, instructions, servings} = props
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
