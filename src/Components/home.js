8aa39875acfa2913d74cc1b9636098c69cec0e5b
vospader13 <justenmcdonald@gmail.com>
2020-02-03 19:37:01 -0700

    fixed cap issue on import

--- src/Components/home.js
+++ src/Components/home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import styled from 'styled-components';
 
-import recipeCard from './recipeCard'
+import RecipeCard from './recipeCard'
 
 const HomeWrapper = styled.div `
     display: flex;
