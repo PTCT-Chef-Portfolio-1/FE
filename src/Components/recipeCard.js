dfa447f775a8a273f7c61840723cb7d365a6d23d
vospader13 <justenmcdonald@gmail.com>
2020-02-03 20:29:58 -0700

    fixed some styling issues to create uniformity on cards.

--- src/Components/recipeCard.js
+++ src/Components/recipeCard.js
@@ -10,15 +10,15 @@ import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
   card: {
+    minWidth: 345,
     maxWidth: 345,
-    border: '1px solid black'
+    margin: 100,
+    // border: '1px solid black'
   },
   media: {
     height: 0,
